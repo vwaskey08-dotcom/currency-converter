5b00e841cd31f53425e3ec04be46cdaba9671ffc
vwaskey08-dotcom <vwaskey08@gmail.com>
2026-05-17 18:26:16 -0700

    Added in the spring files

---
+++ src/main/java/com/example/currency_converter_spring/CurrencyConverterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.currency_converter_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/currency_converter_spring/CurrencyConverterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currency_converter_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
